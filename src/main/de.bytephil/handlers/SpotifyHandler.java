0bab108e387532b9220b9b1239fe643244e104bf
BytePhil <63234545+BytePhilHD@users.noreply.github.com>
2024-07-28 19:06:16 +0000

    Fixed DurationToSong

--- src/main/de.bytephil/handlers/SpotifyHandler.java
+++ src/main/de.bytephil/handlers/SpotifyHandler.java
@@ -9,23 +9,22 @@ public class SpotifyHandler {
     private SpotifyAPIConnector spotifyAPI = new SpotifyAPIConnector();
 
     public int getDurationtoSong(String url) {
+        double lengthInSeconds = 0.0;
         List<IPlaylistItem> userQueue = spotifyAPI.getUsersQueue();
-        int lengthInSeconds = 0;
         
         try {
-            lengthInSeconds += spotifyAPI.getCurrentTrackItem().getDurationMs() / 1000;
+            lengthInSeconds += (double) spotifyAPI.getCurrentTrackItem().getDurationMs() / 1000;
         } catch(Exception e) {
             e.printStackTrace();
         }
         
         for (IPlaylistItem item : userQueue) {
             if (item.getUri().equals(url)) {
-                return (int) Math.ceil(lengthInSeconds / 60.0);
+                return (int) Math.round(lengthInSeconds / 60.0);
             } else {
-                lengthInSeconds += item.getDurationMs() / 1000;
+                lengthInSeconds += (double) item.getDurationMs() / 1000;
             }
         }
-        
-        return -1;
+        return (int) Math.round(lengthInSeconds / 60.0);
     }
-}
+}    
